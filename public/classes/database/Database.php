a794850ac8c3b1617a081dd4b6270450840bdfdc
ANDRE Antoine <totoandre2507@gmail.com>
2025-09-29 09:13:18 +0200

    Update Database

--- public/classes/database/Database.php
+++ public/classes/database/Database.php
@@ -1,16 +1,13 @@
 <?php
-// classes/Database.php
 class Database {
-    private static ?PDO $pdo = null; // statci permet d'y accéder sans créer d'objet
+    private static ?PDO $pdo = null;
 
     private static string $host = "localhost";
     private static string $user = "root";
     private static string $pass = "";
     private static string $dbname = "foot_club";
 
-    private function __construct() {} // Empêche new Database() Pourquoi ?
-    // Cette classe est un singleton : on veut qu’il n’existe qu’une seule connexion PDO pour toute l’application.
-    // Personne ne doit créer plusieurs objets
+    private function __construct() {}
 
     public static function getConnection(): PDO {
         if (self::$pdo === null) {
@@ -28,19 +25,3 @@ class Database {
         return self::$pdo;
     }
 }
-
-
-
-// database include de base
-// <?php
-// $host = "localhost";
-// $user = "root";
-// $pass = "";
-// $dbname = "foot_club";
-
-// try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     echo "Connection failed: " . $e->getMessage();
-// }
